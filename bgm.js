d724397396455f3605c850c47fe3350513b2db21
Akrin724 <akrin.qwq@gmail.com>
2026-09-08 00:53:44 +0800

    更改JS文件路径

--- bgm.js
+++
@@ -1,7 +0,0 @@
-const bgm = document.querySelector("#bgm");
-
-bgm.volume = 0.2;
-
-window.addEventListener("click", () => {
-    bgm.play();
-}, { once: true });
--- navbar.js
+++
@@ -1,9 +0,0 @@
-const navbar = document.getElementById("navbar");
-window.addEventListener("scroll", () => {
-    if (window.scrollY > 300){
-        navbar.classList.add("hidden");
-    }
-    else {
-        navbar.classList.remove("hidden");
-    }
-});
