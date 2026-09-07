c158e355590cca809d1b6a5fc10fa5991283f993
Akrin724 <akrin.qwq@gmail.com>
2026-09-08 01:14:59 +0800

    离开页面自动停止

--- script/bgm.js
+++ script/bgm.js
@@ -5,3 +5,29 @@ bgm.volume = 0.2;
 window.addEventListener("click", () => {
     bgm.play();
 }, { once: true });
+
+// 切到后台
+document.addEventListener("visibilitychange", () => {
+    if (document.hidden) {
+        wasPlaying = !bgm.paused;
+
+        if (wasPlaying) {
+            bgm.pause();
+        }
+    } else {
+        // 回到网页
+        if (wasPlaying) {
+            bgm.play();
+        }
+    }
+});
+
+
+// 页面被隐藏 / 离开
+window.addEventListener("pagehide", () => {
+    wasPlaying = !bgm.paused;
+
+    if (wasPlaying) {
+        bgm.pause();
+    }
+});
